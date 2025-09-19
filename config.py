f5b2eed7b27f46949591060474f747a108d0675a
zer0jek <panpomidoras@gmail.com>
2025-09-19 23:10:05 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Konfiguracja progów punktowych i ID ról Discorda
+# Uzupełnij ID ról zgodnie z Twoim serwerem Discord
+ROLE_THRESHOLDS = [
+    (1000, 1418695474940219492),  # np. 1000+
+    (500, 1418695450709594382),  # np. 500+
+    (100,  1418695425401294909),  # np. 100+
+]
